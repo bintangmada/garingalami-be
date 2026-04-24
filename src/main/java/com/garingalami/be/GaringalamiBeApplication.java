a20f727e53c56600967ebf1d4ee729452c0e06c9
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2026-04-24 15:25:55 +0700

    chore: initialize spring boot project for garing alami backend

---
+++ src/main/java/com/garingalami/be/GaringalamiBeApplication.java
@@ -0,0 +1,13 @@
+package com.garingalami.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaringalamiBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaringalamiBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/garingalami/be/GaringalamiBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garingalami.be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaringalamiBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
